4fb9c6d6b80d475342967e6b7d73e39fdd98af9f
Samantha Pomeroy <samanthapomeroy38@gmail.com>
2024-01-30 12:20:37 -0600

    operators

--- main.js
+++ main.js
@@ -1,13 +1,42 @@
 let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
 
+// if(string.indexOf['/0'] != -1){
+//     string = 'error'
+// }
+
 let string ='';
 let arr = Array.from(buttons);
+let count = 0;
+let operators = document.querySelectorAll('.operator')
+let secondCount = 0;
 arr.forEach(button => {
     button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML === '+' || e.target.innerHTML === '-' || e.target.innerHTML === '*' || e.target.innerHTML === '/'){
+            count++
+            secondCount++
+            if(count === 2 && secondCount <= 2){
+                string = eval(string)
+                input.value = string
+                if(string.indexOf['/0'] != -1){
+                    string = undefined
+                }
+                count=1
+            }
+            else if(count === 2 && secondCount > 2){
+                string = eval(string)
+                input.value = string
+                count=1
+
+            }
+            console.log(count)
+        }
         if(e.target.innerHTML === '='){
             string = eval(string);
             input.value = string;
+            if(string.indexOf['/0'] != -1){
+                string = undefined
+            }
         }
 
         else if(e.target.innerHTML === 'C'){
@@ -20,4 +49,5 @@ arr.forEach(button => {
         }
         
     })
+    
 })
